32de04f0b064537b8f4ed1097e465c1e4db7f971
Mai Văn Long <mv_long@brycen-vn.local>
2023-09-12 08:53:00 +0700

    Message

--- FeAngular/src/app/app-routing.module.ts
+++ FeAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/body/home/home.component';
+import { ProductsComponent } from './Components/body/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'product', component: ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
