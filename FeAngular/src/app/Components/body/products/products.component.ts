e2d8f7a50447aaf1ae11c951d3071425af3e6b9a
Mai Văn Long <mv_long@brycen-vn.local>
2023-09-22 11:30:07 +0700

    JadeDragon

--- FeAngular/src/app/Components/body/products/products.component.ts
+++ FeAngular/src/app/Components/body/products/products.component.ts
@@ -1,4 +1,16 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import ListProduct from '../../../Data/Product.json';
+interface Products{
+  idMenuProducts: string;
+  nameMenuProducts: string;
+  listProduct: {
+    idProduct: number;
+    typeProducts: string;
+    nameProduct: string;
+    moeny: string;
+    urlImg: string
+  }
+}
 
 @Component({
   selector: 'app-products',
@@ -10,26 +22,51 @@ export class ProductsComponent implements OnInit, AfterViewInit {
   p: number = 1;
   GetId: string | null = null;
   selectedMenuData: any[] = [];
+  selectedTypeData: any[] = [];
 
   constructor(){}
+  ClickAll(){
+    if(this.Products.length > 0 ){
+      this.selectedMenuData = [];
 
-  ClickGetId(id: string){
-    this.GetId = id;
-    this.update();
+      for(let category of this.Products){
+        const productList = category.listProduct;
+  
+        for(let productItem of productList){
+            this.selectedMenuData.push(productItem);
+        }
+      }
+    }
   }
 
+  // =======================
   onChage(event: any){
     const se = event.target?.value;
     
     if(se !== undefined){
-      this.ClickGetId(se);
+      this.GetId = se;
+      this.update();
     }
   }
+  onChagee(event: any){
+    const se = event.target?.value;
+    
+    if(se !== undefined){
+      this.GetType(se);
+    }
+  }
+  // ========================
 
+  // ========================
   ngOnInit() {
       if(this.GetId === null){
-        this.GetId = this.Products.length > 0 ? this.Products[0].idMenuProducts : null;
-        this.update();
+        for(let list of this.Products){
+          const category = list.listProduct;
+
+          for(let item of category){
+            this.selectedMenuData.push(item);
+          }
+        }
       } 
   }
 
@@ -39,19 +76,61 @@ export class ProductsComponent implements OnInit, AfterViewInit {
         this.selectedMenuData = tsData;
       }
   }
-  
+  // ==========================
+
+  // ==========================
   update() {
     if(this.GetId !== null){
-      const tsData = this.GetData(this.GetId);
-      this.selectedMenuData = tsData;
-      console.log("selectedMenuData", this.selectedMenuData)
-    }
-}
 
+      if(this.GetId == 'all'){
+        this.selectedMenuData = [];
+
+        for(let category of this.Products){
+          const productList = category.listProduct;
+    
+          for(let productItem of productList){
+              this.selectedMenuData.push(productItem);
+          }
+        }
+      } else{
+        const tsData = this.GetData(this.GetId);
+        this.selectedMenuData = tsData;
+        this.selectedTypeData = this.selectedMenuData;
+      }
+    }
+  }
+  // ==========================
+
+  // ==========================
   GetData(id: string){
     const result = this.Products.find(item => item.idMenuProducts == id);
     return result ? result.listProduct : [];
   }
+
+  GetType(id: string){
+    this.selectedMenuData = [];
+
+    if(id !== 'null'){
+      for(let category of this.Products){
+        const productList = category.listProduct;
+  
+        for(let productItem of productList){
+          if(productItem.typeProducts === id){
+            this.selectedMenuData.push(productItem);
+          }
+        }
+      }
+    } else{
+      for(let category of this.Products){
+        const productList = category.listProduct;
+  
+        for(let productItem of productList){
+          this.selectedMenuData.push(productItem);
+        }
+      }
+    }
+  }
+  // ==========================
 
   Products = [
     {
